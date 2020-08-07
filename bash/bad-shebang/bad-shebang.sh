d71a2b81b115d22dc2e213a9d1d5c051e5b37b40
Douglas Naphas <douglasnaphas@gmail.com>
2020-08-07 13:10:49 -0400

    Show what happens when the shebang is bad

---
+++ bash/bad-shebang/bad-shebang.sh
@@ -0,0 +1,7 @@
+#!/bin/bad-program-bash
+
+echo "bad-shebang was executed"
+
+# ./bad-shebang.sh
+# prints:
+# -bash: ./bad-shebang.sh: /bin/bad-program-bash: bad interpreter: No such file or directory
