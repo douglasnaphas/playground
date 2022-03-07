330b3ba46f98e1c62e48d5ae3a220a88c2f77f0f
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-06 19:00:08 -0500

    Revise the pptr fetch to match my normal fetch
    This is the way I normally phrase fetches.

--- pptr/fetch/index.js
+++ pptr/fetch/index.js
@@ -4,31 +4,17 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // let's navigate to the dev.to homepage
   await page.goto("https://dev.to");
 
-  // evaluate will run the function in the page context
-  // const output = await page.evaluate(() => {
-  //   window
-  //     .fetch("https://jsonplaceholder.typicode.com/comments/1")
-  //     .then((r) => r.json())
-  //     .then((j) => {
-  //       console.log("j:");
-  //       console.log(j);
-  //       return Promise.resolve(j.email);
-  //     });
-  // });
   const output = await page.evaluate(async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/comments/1"
-    );
-    const text = await response.text();
-    return text;
+    const email = await fetch("https://jsonplaceholder.typicode.com/comments/1")
+      .then((r) => r.json())
+      .then((j) => j.email);
+    return email;
   });
 
   console.log("output:");
   console.log(output);
 
-  // we're done; close the browser
   await browser.close();
 })();
