327fe13c33cdd0e16797299b7ee2498f4f8d273b
Douglas Naphas <dnaphas@healthverity.com>
2025-11-20 17:26:17 -0500

    Check dirname -- ${BASH_SOURCE[0]}

--- bash/bash-source-in-scripts.sh
+++ bash/bash-source-in-scripts.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "BASH_SOURCE[0]: ${BASH_SOURCE[0]}"
+echo -n 'dirname -- ${BASH_SOURCE[0]}: ' ; dirname -- ${BASH_SOURCE[0]}
 echo "realpath BASH_SOURCE[0]: $(realpath ${BASH_SOURCE[0]})"
